d0ffe11298b86feacfcd9573f91eee7f8c621c17
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-29 12:17:03 -0800

    Fixed link to local host

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(logger("dev"));
 
+// enable compression middleware
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,5 +25,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port http://localhost:${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
